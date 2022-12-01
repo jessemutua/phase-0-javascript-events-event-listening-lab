d2ecfe5698782d206061932d483363e1dbf60f0b
Jesse <jessemutua76@gmail.com>
2022-12-01 18:43:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let input = document.getElementById('button');
 
 function addingEventListener() {
-    input.addEventListener('click', function() {
-        alert('I was clicked!');
+    input.addEventListener('mouseover', function() {
+        input.style.backgroundColor = "red";
     });
 }
 
